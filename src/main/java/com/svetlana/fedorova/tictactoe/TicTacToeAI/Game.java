1a1f9be91d82990cdef8bfca2558120e57b56bf2
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-09-02 20:24:58 +0300

    4 part finished

--- src/main/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/Game.java
+++ src/main/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/Game.java
@@ -187,14 +187,17 @@ public class Game {
             if (grid[0][0] == grid[1][1] && grid[1][1] == playerChar) {
                 if (grid[2][2] == ' ') {
                     grid[2][2] = inputChar;
+                    isAIMoved = true;
                 }
             } else if (grid[1][1] == grid[2][2] && grid[1][1] == playerChar) {
                 if (grid[0][0] == ' ') {
                     grid[0][0] = inputChar;
+                    isAIMoved = true;
                 }
             } else if (grid[0][0] == grid[2][2] && grid[2][2] == playerChar) {
                 if (grid[1][1] == ' ') {
                     grid[1][1] = inputChar;
+                    isAIMoved = true;
                 }
             }
         }
@@ -203,14 +206,17 @@ public class Game {
             if (grid[2][0] == grid[1][1] && grid[1][1] == playerChar) {
                 if (grid[0][2] == ' ') {
                     grid[0][2] = inputChar;
+                    isAIMoved = true;
                 }
             } else if (grid[1][1] == grid[0][2] && grid[0][2] == playerChar) {
                 if (grid[2][0] == ' ') {
                     grid[2][0] = inputChar;
+                    isAIMoved = true;
                 }
             } else if (grid[2][0] == grid[0][2] && grid[2][0] == playerChar) {
                 if (grid[1][1] == ' ') {
                     grid[1][1] = inputChar;
+                    isAIMoved = true;
                 }
             }
         }
