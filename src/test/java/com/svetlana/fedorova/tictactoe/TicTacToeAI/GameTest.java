6a08801cd71648c9e8ab7a1fcd60c2fcecfa79a3
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-09-09 21:38:30 +0300

    5th part started//hard level works incorrectly

---
+++ src/test/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/GameTest.java
@@ -0,0 +1,24 @@
+package com.svetlana.fedorova.tictactoe.TicTacToeAI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void getGameParam() {
+    }
+
+    @Test
+    void isCellOccupied() {
+    }
+
+    @Test
+    void playGame() {
+    }
+
+    @Test
+    void getGameResult() {
+    }
+}
