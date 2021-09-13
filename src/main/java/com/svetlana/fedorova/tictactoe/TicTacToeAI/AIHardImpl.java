c08dea09c3afa2d00eb2e4232226eebcae8525dc
Svetlana Fedorova <svetlana.fedorova@hotmail.com>
2021-09-13 16:56:48 +0300

    5th part started//hard level works incorrectly

--- src/main/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/AIHardImpl.java
+++ src/main/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/AIHardImpl.java
@@ -1,91 +1,115 @@
 package com.svetlana.fedorova.tictactoe.TicTacToeAI;
 
-import static com.svetlana.fedorova.tictactoe.TicTacToeAI.Game.currentState;
 import static com.svetlana.fedorova.tictactoe.TicTacToeAI.Game.grid;
-import static com.svetlana.fedorova.tictactoe.TicTacToeAI.State.DRAW;
-import static com.svetlana.fedorova.tictactoe.TicTacToeAI.State.ONGOING_GAME;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class AIHardImpl extends AI {
 
-    Game game = new Game();
+    private static char hardPlayer;
+    private static char nonHardPlayer;
 
     @Override
     public void move(Terminal terminal, char playerChar) {
-        int[] move = getBestMove(playerChar);
-        grid[move[0]][move[1]] = playerChar;
-    }
+        hardPlayer = playerChar;
+        nonHardPlayer = hardPlayer == 'X' ? 'O' : 'X';
 
-    private int[] getBestMove(char playerChar) {
-        int[] bestMove = new int[2];
-        int bestValue = Integer.MIN_VALUE;
+        int[] originalBoard = new int[9];
+        int n = 0;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (grid[i][j] == ' ') {
-                    grid[i][j] = playerChar;
-                    int moveValue = miniMax(false, playerChar);
-                    grid[i][j] = ' ';
-                    currentState = ONGOING_GAME;
-                    if (moveValue > bestValue) {
-                        bestValue = moveValue;
-                        bestMove[0] = i;
-                        bestMove[1] = j;
-                    }
+                if (grid[i][j] != ' ') {
+                    originalBoard[n] = grid[i][j];
+                } else {
+                    originalBoard[n] = n;
+                }
+                n++;
+                if (n == 9) {
+                    break;
+                }
+            }
+        }
+        int move = miniMax(originalBoard, hardPlayer);
+        int k = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (k == move) {
+                    grid[i][j] = hardPlayer;
+                    k++;
+                }
+            }
+        }
+    }
+
+    private int[] emptyIndices(int[] board) {
+        return Arrays.stream(board).filter(e -> e != 'X' && e != 'O')
+            .toArray();
+    }
+
+    private boolean winning(int[] board, char player) {
+        return (board[0] == player && board[1] == player && board[2] == player) ||
+            (board[3] == player && board[4] == player && board[5] == player) ||
+            (board[6] == player && board[7] == player && board[8] == player) ||
+            (board[0] == player && board[3] == player && board[6] == player) ||
+            (board[1] == player && board[4] == player && board[7] == player) ||
+            (board[2] == player && board[5] == player && board[8] == player) ||
+            (board[0] == player && board[4] == player && board[8] == player) ||
+            (board[2] == player && board[4] == player && board[6] == player);
+    }
+
+    private int miniMax(int[] newBoard, char player) {
+        int[] availSpots = emptyIndices(newBoard);
+
+        if (winning(newBoard, nonHardPlayer)) {
+            return -10;
+        } else if (winning(newBoard, hardPlayer)) {
+            return 10;
+        } else if (availSpots.length == 0) {
+            return 0;
+        }
+
+        Map<Integer, Integer> moves = new HashMap<>();
+        for (int availSpot : availSpots) {
+            moves.put(newBoard[availSpot], null);
+            int temp = newBoard[availSpot];
+            newBoard[availSpot] = player;
+            int result;
+            if (player == hardPlayer) {
+                result = miniMax(newBoard, nonHardPlayer);
+            } else {
+                result = miniMax(newBoard, hardPlayer);
+            }
+            moves.put(availSpot, result);
+            newBoard[availSpot] = temp;
+        }
+        int bestMove = 0;
+        if (player == hardPlayer) {
+            int bestScore = -1000;
+            int maxValue = Collections.max(moves.values());
+            if (maxValue > bestScore) {
+                bestScore = maxValue;
+            }
+            for (Entry<Integer, Integer> entry : moves.entrySet()) {
+                if (entry.getValue() == maxValue) {
+                    bestMove = entry.getKey();
+                }
+            }
+        } else {
+            int bestScore = 1000;
+            int minValue = Collections.min(moves.values());
+            if (minValue < bestScore) {
+                bestScore = minValue;
+            }
+            for (Entry<Integer, Integer> entry : moves.entrySet()) {
+                if (entry.getValue() == minValue) {
+                    bestMove = entry.getKey();
                 }
             }
         }
         return bestMove;
     }
-
-    private int miniMax(boolean isMax, char playerChar) {
-        int boardVal = winning(playerChar);
-        if (boardVal == 10 || boardVal == -10 || boardVal == 0) {
-            return boardVal;
-        }
-
-        if (isMax) {
-            int highestValue = Integer.MIN_VALUE;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (grid[i][j] == ' ') {
-                        grid[i][j] = playerChar;
-                        highestValue = Math.max(highestValue,
-                            miniMax(false, playerChar));
-                        grid[i][j] = ' ';
-                        currentState = ONGOING_GAME;
-                    }
-                }
-            }
-            return highestValue;
-        } else {
-            int lowestValue = Integer.MAX_VALUE;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (grid[i][j] == ' ') {
-                        if (playerChar == 'X') {
-                            grid[i][j] = 'O';
-                        } else {
-                            grid[i][j] = 'X';
-                        }
-                        lowestValue = Math.min(lowestValue, miniMax(true, playerChar));
-                        grid[i][j] = ' ';
-                        currentState = ONGOING_GAME;
-                    }
-                }
-            }
-            return lowestValue;
-        }
-    }
-
-    private int winning(char playerChar) {
-        char winner = game.getGameResult();
-        if (winner == playerChar) {
-            return 10;
-        } else if (winner != playerChar && winner != ' ') {
-            return -10;
-        } else if (winner == ' ' && currentState == DRAW) {
-            return 0;
-        }
-        return 2;
-    }
 }
-
--- src/test/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/GameTest.java
+++ src/test/java/com/svetlana/fedorova/tictactoe/TicTacToeAI/GameTest.java
@@ -1,13 +1,25 @@
 package com.svetlana.fedorova.tictactoe.TicTacToeAI;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.svetlana.fedorova.tictactoe.TicTacToeAI.Game.currentState;
+import static com.svetlana.fedorova.tictactoe.TicTacToeAI.Game.xAmount;
+import static com.svetlana.fedorova.tictactoe.TicTacToeAI.State.ONGOING_GAME;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
 class GameTest {
 
     @Test
-    void getGameParam() {
+    void getCorrectParam() {
+        String input = "start user easy";
+        assertThat(currentState).isSameAs(ONGOING_GAME);
+        assertThat(xAmount).isEqualTo(0);
+    }
+
+    @Test
+    void getIncorrectParam() {
+        String input = "start user easy";
+
     }
 
     @Test
@@ -16,6 +28,7 @@ class GameTest {
 
     @Test
     void playGame() {
+
     }
 
     @Test
